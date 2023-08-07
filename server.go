dcd0fbe1cfe9a78682bec455767b3a53173b8d3f
Carlos Chaves <carlosdiegodev@gmail.com>
2023-08-07 16:39:24 -0300

    Atualizando arquivos do projeto.

---
+++ server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/family", ConfigMap)
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":8000", nil)
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	fmt.Fprintf(w, "Hello, I'm %s", name)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("Error reading file.", err)
+	}
+	fmt.Fprintf(w, "My Family: %s.", string(data))
+}
